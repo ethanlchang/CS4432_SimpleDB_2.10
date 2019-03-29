d31eb0db8cac139263fcd1ffe843cb0a8c52ec8e
Ethan Chang <elchang@wpi.edu>
2019-03-29 18:23:05 -0400

    Part 1: Examples
    added SQL queries for both tables

--- Examples/Examples.java
+++ Examples/Examples.java
@@ -8,14 +8,22 @@ public class Examples {
             Driver d = new SimpleDriver();
             conn = d.connect("jdbc:simpledb://localhost", null);
             Statement stmt = conn.createStatement();
+            String s;
 
+            //Clear out SQL Tables
+            s = "delete from PERSON ";
+            stmt.executeUpdate(s);
+            System.out.println("Table PERSON deleted.");
+            s = "delete from ACCOUNT ";
+            stmt.executeUpdate(s);
+            System.out.println("Table ACCOUNT deleted.");
 
             /* Person
             SSN - int
             Name - vachar(50)
             AccountNumber - int
              */
-            String s = "create table PERSON(SSN int, Name varchar(50), AccountNumber int)";
+            s = "create table PERSON(SSN int, Name varchar(50), AccountNumber int)";
             stmt.executeUpdate(s);
             System.out.println("Table PERSON created.");
 
@@ -53,6 +61,52 @@ public class Examples {
 
 
             //SQL queries
+            String qry;
+            ResultSet rs;
+
+            //Find 'John Smith'
+            System.out.println("\nFull name of John");
+            qry = "select name "
+                    + "from person "
+                    + "where name =  'John Smith'";
+            rs = stmt.executeQuery(qry);
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                System.out.println("John's name is: " + name);
+            }
+            rs.close();
+
+            //Find all accounts in branch 'A' with balance 47
+            System.out.println("\nAccounts in branch A with balance of 47");
+            qry = "select accountnumber, balance, bankbranch "
+                    + "from account "
+                    + "where bankbranch =  'A' "
+                    + "and balance = 47 ";
+            rs = stmt.executeQuery(qry);
+
+            while (rs.next()) {
+                int accountNum = rs.getInt("accountnumber");
+                int balance = rs.getInt("balance");
+                String branch = rs.getString("bankbranch");
+                System.out.println("Account Number: " + accountNum + "\nBalance: "  + balance + "\nBranch: " + branch);
+            }
+            rs.close();
+
+            //Find all accounts in branch 'C'
+            System.out.println("\nAccounts in branch C");
+            qry = "select accountnumber, balance, bankbranch "
+                    + "from account "
+                    + "where bankbranch =  'C' ";
+            rs = stmt.executeQuery(qry);
+
+            while (rs.next()) {
+                int accountNum = rs.getInt("accountnumber");
+                int balance = rs.getInt("balance");
+                String branch = rs.getString("bankbranch");
+                System.out.println("Account Number: " + accountNum + "\nBalance: "  + balance + "\nBranch: " + branch);
+            }
+            rs.close();
 
         }
         catch(SQLException e) {
